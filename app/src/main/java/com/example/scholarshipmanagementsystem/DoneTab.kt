d8d93abeff465747b9d9570711445b75bc501973
princessgaela <prga.gaela.up@phinmaed.com>
2022-11-16 02:13:45 +0800

    upload

---
+++ app/src/main/java/com/example/scholarshipmanagementsystem/DoneTab.kt
@@ -0,0 +1,11 @@
+package com.example.scholarshipmanagementsystem
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DoneTab : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.done_tab)
+    }
+}
